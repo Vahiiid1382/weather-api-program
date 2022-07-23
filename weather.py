a1fe9455c425b8cda082d51ff21784c21d924c73
vahid <whydabrahymy390@gmail.com>
2022-07-23 00:42:19 -0700

    weather welcome end

--- weather.py
+++ weather.py
@@ -1,20 +1,35 @@
 import requests
 import json
 import pprint
-city_name = input('witch city do tou want to know?')
-url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=13b9ce10cc8d9a63336857063b618625'
-result1 = requests.get(url)
-result2 = json.loads(result1.text)
-#print(result2)
+flag = True
+print('Hello welcome to weather app we have very information about weather of citys around the world!!!')
+while flag:
+    
+    city_name = input('witch city do tou want to know?')
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=13b9ce10cc8d9a63336857063b618625'
+    result1 = requests.get(url)
+    result2 = json.loads(result1.text)
+    #print(result2)
 
-weather_main = result2['weather'][0]['description']
-now_temp = float(result2['main']['temp']) - 273.15
-humity = str(result2['main']['humidity']) + '%'
-wind_speed = str(result2['wind']['speed']) + 'm/s'
-print(weather_main)
-print(now_temp)
-print(humity)
-print(wind_speed)
+    weather_main = result2['weather'][0]['description']
+    now_temp = float(result2['main']['temp']) - 273.15
+    humity = str(result2['main']['humidity']) + '%'
+    wind_speed = str(result2['wind']['speed']) + 'm/s'
+    print(weather_main)
+    print(now_temp)
+    print(humity)
+    print(wind_speed)
+    while True:
+        edame = input('Do you want to continue?y/n')
+        if edame == 'y':
+            break
+        elif edame == 'n':
+            flag = False
+            break
+        else:
+            print('Please enter right!!!y/n')
+            continue
+print('.....END........')
 '''
 '''
 '''
