9d25380e2158580ee27cd12c17bbb7fbd2a49955
vahid <whydabrahymy390@gmail.com>
2022-07-23 00:52:50 -0700

    weather added string

--- weather.py
+++ weather.py
@@ -15,10 +15,10 @@ while flag:
     now_temp = float(result2['main']['temp']) - 273.15
     humity = str(result2['main']['humidity']) + '%'
     wind_speed = str(result2['wind']['speed']) + 'm/s'
-    print(weather_main)
-    print(now_temp)
-    print(humity)
-    print(wind_speed)
+    print('Weather dis: ',weather_main)
+    print('Right now temp: ',now_temp)
+    print('Humidity percent: ',humity)
+    print('Wind-speed: ',wind_speed)
     while True:
         edame = input('Do you want to continue?y/n')
         if edame == 'y':
